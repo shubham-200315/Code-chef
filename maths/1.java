4a72c72d5c53fc64ac2f7c462a59ae3ad5fabcca
shubham-200315 <105916104+shubham-200315@users.noreply.github.com>
2022-11-20 21:19:19 +0530

    Create 1.java

---
+++ maths/1.java
@@ -0,0 +1,39 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner (System.in);
+	  int t = sc.nextInt();
+	  for(int i=0; i<t; i++)
+	  {
+	    
+		int n = sc.nextInt();
+		int f = fact(n);
+		System.out.println(f);
+	  }
+		
+	}
+	
+	public static int fact(int n)
+	{
+	      if(n==0)
+	    
+	      return 1;   
+	    
+	       
+	    else
+	    
+	        
+	    return ( n* fact(n-1));    
+	    
+
+	}
+}
