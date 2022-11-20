27a66e80f93d76db947eabbe3f493480500e92ca
shubham-200315 <105916104+shubham-200315@users.noreply.github.com>
2022-11-20 22:04:57 +0530

    Create 1.java

---
+++ String/1.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int T=sc.nextInt();
+	 while(T-->0){
+	     int N=sc.nextInt();
+	     int count=0;
+	     for(int i=0;i<N;i++){
+	         String s=sc.next();
+	         if(s.equals("START38")){
+	             count++;
+	         }
+	     }
+	     System.out.println(count+" "+(N-count));
+	 }
+	}
+}
